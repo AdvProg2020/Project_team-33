d0c0b5a269295fcff57bf1d05f5c493c1933c63d
mmdrez4 <mohammadreza.dn80@gmail.com>
2020-05-02 23:57:19 +0430

    write parent Class for Menus

--- src/main/java/View/Menu.java
+++ src/main/java/View/Menu.java
@@ -1,5 +1,43 @@
 package View;
 
+import Model.Manager;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Menu {
+    private String name;
+    protected Menu previousMenu;
+    protected ArrayList<Menu> subMenus;
+    public static Scanner scanner;
+    protected Manager manager;
+    protected ArrayList<Menu> allMenus;
+
+    public Menu(String name, Menu previousMenu) {
+        this.name = name;
+        this.previousMenu = previousMenu;
+        subMenus = new ArrayList<Menu>();
+        allMenus = new ArrayList<Menu>();
+    }
+
+    public void show(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setSubMenus(ArrayList<Menu> subMenus) {
+        this.subMenus = subMenus;
+    }
+
+    public void execute(){
+
+    }
+
+    public void commandProcessor(){
+
+    }
 
 }
