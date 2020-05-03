32b25743f5cab27f43c09e9d61960c61024e6719
AmirMahdi <amk_amir82@yahoo.com>
2020-05-04 00:18:12 +0430

    Register menu first commit

---
+++ src/main/java/View/RegisterMenu.java
@@ -0,0 +1,20 @@
+package View;
+
+import Model.Person;
+
+public class RegisterMenu {
+    private Person person;
+    private long money;
+
+    public void createAccountProcess(){
+
+    }
+
+    public void commandProcess(){
+
+    }
+
+    public void help(){
+
+    }
+}
