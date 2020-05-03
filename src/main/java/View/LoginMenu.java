1258f4b68be8007ce4810da2ef7b673fcfccd490
AmirMahdi <amk_amir82@yahoo.com>
2020-05-04 00:17:19 +0430

    Manager menu first commit

---
+++ src/main/java/View/LoginMenu.java
@@ -0,0 +1,20 @@
+package View;
+
+import Model.Person;
+
+public class LoginMenu {
+    private Person person;
+
+    public void loginProcess() {
+
+    }
+
+    public void commandProcess(){
+
+    }
+
+    public void help(){
+
+    }
+
+}
