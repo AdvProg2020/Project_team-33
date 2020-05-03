7d2a3fcca55b7c889441a9c95860b2d836f4db7e
AmirMahdi <amk_amir82@yahoo.com>
2020-05-04 00:30:10 +0430

    Request first commit

---
+++ src/main/java/Model/Request.java
@@ -0,0 +1,19 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class Request {
+
+    public static void addRequest(Request request){
+
+    }
+
+    public static void deleteRequest(Request request){
+
+    }
+
+    public static Request getRequest(int i){
+        return null;
+    }
+
+}
