bf2b4bde1680962d832130e40836f27b3c201750
AmirMahdi <amk_amir82@yahoo.com>
2020-05-04 04:34:36 +0430

    login process

--- src/main/java/View/LoginMenu.java
+++ src/main/java/View/LoginMenu.java
@@ -2,19 +2,37 @@ package View;
 
 import Model.Person;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class LoginMenu {
     private Person person;
+    private static Matcher matcher;
 
-    public void loginProcess() {
+    public void loginProcess(Matcher matcher) {
+        String username=matcher.group(2);
+        System.out.println("password: ");
+        String password=Menu.scanner.nextLine();
 
     }
 
-    public void commandProcess(){
+    public void commandProcess() {
+
+        if ((matcher = getMatcher(Menu.scanner.nextLine(), "(login )(\\S)")).find()) {
+            loginProcess(matcher);
+        } else {
+            System.out.println("invalid command");
+        }
+    }
+
+    public void help() {
 
     }
 
-    public void help(){
-
+    private static Matcher getMatcher(String input, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        return matcher;
     }
 
 }
