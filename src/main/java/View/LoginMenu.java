dda3e6ae90559cb0923892b6c5ac3c2ad8c8501a
AmirMahdi <amk_amir82@yahoo.com>
2020-05-05 04:17:58 +0430

    login process

--- src/main/java/View/LoginMenu.java
+++ src/main/java/View/LoginMenu.java
@@ -1,21 +1,36 @@
 package View;
 
+import Controller.RegisterProcess;
 import Model.Person;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LoginMenu {
+    public static Person currentPerson;
     private Person person;
     private static Matcher matcher;
 
     public void loginProcess(Matcher matcher) {
         String username = matcher.group(2);
-        System.out.println("password: ");
-        String password = Menu.scanner.nextLine();
-        person = Person.getPersonByUsername(username);
-        if (!person.getPassword().equals(password)) {
-            System.out.println("password incorrect");
+        if (RegisterProcess.usernameTypeErr(username)) {
+            if (Person.getPersonByUsername(username) == null) {
+                System.out.println("password: ");
+                String password = Menu.scanner.nextLine();
+                if (RegisterProcess.passwordTypeErr(password)) {
+                    if ((person = RegisterProcess.login(username, password)) != null) {
+                        currentPerson = person;
+                    } else {
+                        System.out.println("password incorrect");
+                    }
+                } else {
+                    System.out.println("password type incorrect");
+                }
+            } else {
+                System.out.println("there is no user exist with this username");
+            }
+        } else {
+            System.out.println("username type incorrect");
         }
     }
 
@@ -34,8 +49,7 @@ public class LoginMenu {
 
     private static Matcher getMatcher(String input, String regex) {
         Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(input);
-        return matcher;
+        return pattern.matcher(input);
     }
 
 }
