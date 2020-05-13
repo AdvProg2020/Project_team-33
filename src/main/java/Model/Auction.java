fc300ecd923fce3cfdb99bd1eca476987168484c
mmdrez4 <mohammadreza.dn80@gmail.com>
2020-05-13 17:34:33 +0430

    write Auction's fields

--- src/main/java/Model/Auction.java
+++ src/main/java/Model/Auction.java
@@ -1,5 +1,14 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Auction {
+    public ArrayList<Auction> allAuctions;
+    private Date beginningTime;
+    private Date finishingTime;
+    private ArrayList<Product> products;
+    private AuctionStatue auctionStatue;
+    private int discount;
 
 }
