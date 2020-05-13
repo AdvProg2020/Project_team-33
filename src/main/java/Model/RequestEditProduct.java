bb5b44a653ad6d59a52f270da813c55453322d7a
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-05-13 20:59:45 +0430

    edit RequestEditProduct

--- src/main/java/Model/RequestEditProduct.java
+++ src/main/java/Model/RequestEditProduct.java
@@ -7,8 +7,8 @@ public class RequestEditProduct extends Request {
     private Product product;
     public static ArrayList<RequestEditProduct> allAddProductRequest = new ArrayList<RequestEditProduct>();
 
-    public RequestEditProduct(Seller seller, Product product) {
-        super(seller, product, null);
+    public RequestEditProduct(Seller seller, Product product,Auction auction) {
+        super(seller, product, auction);
         this.seller = seller;
         this.product = product;
     }
