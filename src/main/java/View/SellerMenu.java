b853a34380b4d1840cd9c72431db0367c37a92f1
mmdrez4 <mohammadreza.dn80@gmail.com>
2020-05-15 13:54:33 +0430

    write viewSalesHistory method for SellerMenu

--- src/main/java/View/SellerMenu.java
+++ src/main/java/View/SellerMenu.java
@@ -1,6 +1,8 @@
 package View;
 
+import Model.BuyLog;
 import Model.Product;
+import Model.SellLog;
 import Model.Seller;
 
 import java.util.regex.Matcher;
@@ -16,19 +18,50 @@ public class SellerMenu extends Menu {
 
 
     public void viewPersonalInfo() {
-
+        Matcher matcher;
+        while (true) {
+            String input = Menu.scanner.nextLine();
+            if ((matcher = getMatcher(input, "(edit (\\S+))")).find()) {
+                editPersonalInfoProcess(matcher.group(2));
+            } else if (input.equalsIgnoreCase("back")) {
+                return;
+            } else {
+                System.out.println("invalid command");
+            }
+        }
     }
 
-    public void editPersonalInfoProcess() {
-
+    public void editPersonalInfoProcess(String field) {
+        if (field.equalsIgnoreCase("email")){
+            seller.changeEmail(scanner.nextLine());
+        } else if (field.equalsIgnoreCase("password")){
+            seller.changePassword(scanner.nextLine());
+        } else if (field.equalsIgnoreCase("name")){
+            seller.changeName(scanner.nextLine());
+        } else if (field.equalsIgnoreCase("phone")){
+            seller.changePhone(scanner.nextLine());
+        } else if (field.equalsIgnoreCase("family")){
+            seller.changeFamily(scanner.nextLine());
+        } else if (field.equalsIgnoreCase("username")){
+            System.out.println("you can not change your username");
+        } else {
+            System.out.println("invalid field");
+        }
     }
 
     public void viewCompanyInformation() {
-
+        System.out.println(seller.getDescription());
     }
 
     public void viewSalesHistory() {
-
+        int logsNo = 1;
+        for (SellLog log : seller.getLogs()) {
+            System.out.println("sellLog number " + logsNo + ":");
+            for (Product product : log.getProducts()) {
+                System.out.println("\t" + product.getName());
+            }
+            logsNo++;
+        }
     }
 
     public void manageProducts() {
