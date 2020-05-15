df34681a1c5436398cc54848a8a4c66b3675294d
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-05-15 16:33:07 +0430

    Discount

---
+++ src/main/java/Model/Discount.java
@@ -0,0 +1,43 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Discount {
+    ArrayList<Discount> allDiscounts = new ArrayList<Discount>();
+    private int discount;
+    private int maxDiscount;
+    private HashMap<Buyer, Integer> buyerHasThisDiscount = new HashMap<Buyer, Integer>();
+    private int numberBuyerCanUseCode;
+    private String code;
+
+    public Discount(int discount, int maxDiscount, String code) {
+        this.discount = discount;
+        this.maxDiscount = maxDiscount;
+        this.code = code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public int getMaxDiscount() {
+        return maxDiscount;
+    }
+
+    public boolean isDiscountActive(String code) {
+
+    }
+
+    public boolean isThisCodeExist(String code) {
+
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
