6459426ac032d391e7f9ae8f3aa90a3983bdc116
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-05-17 23:13:15 +0430

    edit methods

--- src/main/java/View/SellerMenu.java
+++ src/main/java/View/SellerMenu.java
@@ -13,6 +13,64 @@ public class SellerMenu extends Menu {
         super("Seller Menu");
     }
 
+    public void help() {
+        System.out.println("Enter your command:");
+        System.out.println("1.view personal info");
+        System.out.println("\tedit [field]");
+        System.out.println("2.view company information");
+        System.out.println("3.view sales history");
+        System.out.println("4.manage products");
+        System.out.println("\tview [productId]");
+        System.out.println("\tview buyers [productId]");
+        System.out.println("\tedit [productId]");
+        System.out.println("5.add product");
+        System.out.println("6.remove product");
+        System.out.println("7.show categories");
+        System.out.println("8.view offs");
+        System.out.println("\tview [offId]");
+        System.out.println("\tedit [offId]");
+        System.out.println("\tadd off");
+        System.out.println("9.view balance");
+        System.out.println("back");
+        if (LoginMenu.currentPerson == null) {
+            System.out.println("logout");
+        }
+        System.out.println("exit");
+
+        commandProcess();
+    }
+
+    public void commandProcess() {
+        Matcher matcher;
+        while (true) {
+            String input = Menu.scanner.nextLine();
+            if (input.equalsIgnoreCase("view personal info")) {
+                viewPersonalInfo();
+            } else if (input.equalsIgnoreCase("view company information")) {
+                viewCompanyInformation();
+            } else if (input.equalsIgnoreCase("view sales history")) {
+                viewSalesHistory();
+            } else if (input.equalsIgnoreCase("manage products")) {
+                manageProducts();
+            } else if (input.equalsIgnoreCase("add product")) {
+                addProductProcess();
+            } else if ((matcher = getMatcher(input, "(remove product (\\d+))")).find()) {
+                removeProductProcess(Integer.parseInt(matcher.group(1)));
+            } else if (input.equalsIgnoreCase("show categories")) {
+                showCategories();
+            } else if (input.equalsIgnoreCase("view offs")) {
+                viewOffs();
+            } else if (input.equalsIgnoreCase("help")) {
+                help();
+            } else if (input.equalsIgnoreCase("back")) {
+                Menu.show();
+            } else if (input.equalsIgnoreCase("Exit")) {
+                System.exit(1);
+            } else {
+                System.out.println("invalid command");
+            }
+        }
+    }
 
     public void viewPersonalInfo() {
         Matcher matcher;
@@ -154,42 +212,6 @@ public class SellerMenu extends Menu {
 
     public void viewBalance() {
 
-    }
-
-    public void help() {
-
-    }
-
-    public void commandProcess() {
-        Matcher matcher;
-        while (true) {
-            String input = Menu.scanner.nextLine();
-            if (input.equalsIgnoreCase("view personal info")) {
-                viewPersonalInfo();
-            } else if (input.equalsIgnoreCase("view company information")) {
-                viewCompanyInformation();
-            } else if (input.equalsIgnoreCase("view sales history")) {
-                viewSalesHistory();
-            } else if (input.equalsIgnoreCase("manage products")) {
-                manageProducts();
-            } else if (input.equalsIgnoreCase("add product")) {
-                addProductProcess();
-            } else if ((matcher = getMatcher(input, "(remove product (\\d+))")).find()) {
-                removeProductProcess(Integer.parseInt(matcher.group(1)));
-            } else if (input.equalsIgnoreCase("show categories")) {
-                showCategories();
-            } else if (input.equalsIgnoreCase("view offs")) {
-                viewOffs();
-            } else if (input.equalsIgnoreCase("help")) {
-                help();
-            } else if (input.equalsIgnoreCase("back")) {
-                Menu.show();
-            } else if (input.equalsIgnoreCase("Exit")) {
-                System.exit(1);
-            } else {
-                System.out.println("invalid command");
-            }
-        }
     }
 
     private static Matcher getMatcher(String input, String regex) {
