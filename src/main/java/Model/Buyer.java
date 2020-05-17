6ed8d2dd4c82c6b2150c9c48333909e25df504bd
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-05-18 02:50:05 +0430

    complete most methods

--- src/main/java/Model/Buyer.java
+++ src/main/java/Model/Buyer.java
@@ -4,10 +4,9 @@ import java.util.ArrayList;
 
 public class Buyer extends Person {
     private Cart userCart;
-    private ArrayList<BuyLog> logs = new ArrayList<BuyLog>();
-    public static ArrayList<Buyer> allBuyers = new ArrayList<Buyer>();
-    private ArrayList<String> discountCode = new ArrayList<String>();
-    private ArrayList<Cart> allCarts = new ArrayList<Cart>();
+    private ArrayList<BuyLog> logs = new ArrayList<>();
+    public static ArrayList<Buyer> allBuyers = new ArrayList<>();
+    private ArrayList<String> discountCode = new ArrayList<>();
     private long money;
 
     public Buyer(String name, String family, String username, String password, String phone, String email,
@@ -20,24 +19,6 @@ public class Buyer extends Person {
 
     public Cart getUserCart() {
         return userCart;
-    }
-
-    public void setUserCart(Cart userCart) {
-        this.userCart = userCart;
-    }
-
-    public static boolean isBuyerWithThisNameExist(String name) {
-        for (Buyer buyer : allBuyers) {
-            if (buyer.getName().equals(name)) return true;
-        }
-        return false;
-    }
-
-    public static Buyer getBuyerName(String name) {
-        for (Buyer buyer : allBuyers) {
-            if (buyer.getName().equals(name)) return buyer;
-        }
-        return null;
     }
 
     public void addDiscountCode(String code) {
