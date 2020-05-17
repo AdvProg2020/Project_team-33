76a3455ed1705210afd30acc6b91ce0df5f50135
mmdrez4 <mohammadreza.dn80@gmail.com>
2020-05-17 11:30:33 +0430

    update method manageUsers in ManagerMenu

--- src/main/java/View/ManagerMenu.java
+++ src/main/java/View/ManagerMenu.java
@@ -24,9 +24,6 @@ public class ManagerMenu extends Menu {
             String input = Menu.scanner.nextLine();
             if (input.equalsIgnoreCase("view personal info")) {
                 showPersonalInfo();
-            } else if ((matcher = getMatcher(input, "(edit )((password||name||family||" +
-                    "email||phone)")).find()) {
-                editPersonalInfoProcess(matcher);
             } else if (input.equalsIgnoreCase("manage users")) {
                 manageUsers();
             } else if ((matcher = getMatcher(input, "(view )(\\S+)")).find()) {
@@ -85,143 +82,156 @@ public class ManagerMenu extends Menu {
 
     public void manageUsers() {
         showPeople();
-        while ()
+        Matcher matcher;
+        while (true) {
+            String input = Menu.scanner.nextLine();
+            if ((matcher = getMatcher(input, "(edit )((password||name||family||" +
+                    "email||phone)")).find()) {
+                editPersonalInfoProcess(matcher);
+            } else if (input.equalsIgnoreCase("back")) {
+                Menu.show();
+            } else if (input.equalsIgnoreCase("exit")) {
+                System.exit(1);
+            } else {
+                System.out.println("invalid command");
+            }
+        }
     }
 
-    private void showPersonalInfo() {
-        manager.toString();
-    }
+        private void showPersonalInfo () {
+            manager.toString();
+        }
 
-    private void editPersonalInfoProcess(Matcher matcher) {
-        System.out.println("new " + matcher.group(2) + ":");
-        String field = Menu.scanner.nextLine();
-        if (matcher.group(2).equalsIgnoreCase("password")) {
-            if (!PersonController.checkLengthOfPassWord(field)) {
-                System.out.println("password type incorrect");
+        private void editPersonalInfoProcess (Matcher matcher){
+            System.out.println("new " + matcher.group(2) + ":");
+            String field = Menu.scanner.nextLine();
+            if (matcher.group(2).equalsIgnoreCase("password")) {
+                if (!PersonController.checkLengthOfPassWord(field)) {
+                    System.out.println("password type incorrect");
+                    return;
+                }
+            } else if (matcher.group(2).equalsIgnoreCase("name")) {
+                if (!PersonController.nameTypeErr(field)) {
+                    System.out.println("name type incorrect");
+                    return;
+                }
+            } else if (matcher.group(2).equalsIgnoreCase("phone")) {
+                if (!PersonController.phoneTypeErr(field)) {
+                    System.out.println("phone type incorrect");
+                    return;
+                }
+            } else if (matcher.group(2).equalsIgnoreCase("email")) {
+                if (!PersonController.emailTypeErr(field)) {
+                    System.out.println("email type incorrect");
+                    return;
+                }
+            }
+            PersonController.editPersonalInfo(manager, matcher.group(2), field);
+            System.out.println(matcher.group(2) + " changed successfully");
+        }
+
+        private void showPeople () {
+            ArrayList<Person> people = new ArrayList<Person>();
+            people.addAll(Person.people);
+            for (Person person : people) {
+                System.out.println(person.getUsername());
+            }
+        }
+
+        private void viewPerson (String username){
+            if (!PersonController.usernameTypeErr(username)) {
+                System.out.println("username type incorrect");
+                return;
+            } else if ((person = Person.getPersonByUsername(username)) == null) {
+                System.out.println("there is no user with this username exist");
                 return;
             }
-        } else if (matcher.group(2).equalsIgnoreCase("name")) {
-            if (!PersonController.nameTypeErr(field)) {
+            person.toString();
+        }
+
+        private void deleteUser (String username){
+            if (!PersonController.usernameTypeErr(username)) {
+                System.out.println("username type incorrect");
+                return;
+            } else if ((person = Person.getPersonByUsername(username)) == null) {
+                System.out.println("there is no user with this username exist");
+                return;
+            }
+            Person.deleteUser(username);
+            System.out.println("user deleted successfully");
+        }
+
+        private void createManager () {
+            System.out.println("name: ");
+            String name = Menu.scanner.nextLine();
+            if (!PersonController.nameTypeErr(name)) {
                 System.out.println("name type incorrect");
                 return;
             }
-        } else if (matcher.group(2).equalsIgnoreCase("phone")) {
-            if (!PersonController.phoneTypeErr(field)) {
+            System.out.println("family: ");
+            String family = Menu.scanner.nextLine();
+            System.out.println("username: ");
+            String username = Menu.scanner.nextLine();
+            if (!PersonController.nameTypeErr(username)) {
+                System.out.println("username type incorrect");
+                return;
+            }
+            System.out.println("password: ");
+            String password = Menu.scanner.nextLine();
+            if (!PersonController.checkLengthOfPassWord(password)) {
+                System.out.println("password type incorrect");
+                return;
+            }
+            System.out.println("phone: ");
+            String phone = Menu.scanner.nextLine();
+            if (!PersonController.phoneTypeErr(phone)) {
                 System.out.println("phone type incorrect");
                 return;
             }
-        } else if (matcher.group(2).equalsIgnoreCase("email")) {
-            if (!PersonController.emailTypeErr(field)) {
+            System.out.println("email: ");
+            String email = Menu.scanner.nextLine();
+            if (!PersonController.emailTypeErr(email)) {
                 System.out.println("email type incorrect");
                 return;
             }
+            manager = new Manager(name, family, username, password, phone, email);
+            System.out.println("manager created successfully");
         }
-        PersonController.editPersonalInfo(manager, matcher.group(2), field);
-        System.out.println(matcher.group(2) + " changed successfully");
-    }
 
-    private void showPeople() {
-        ArrayList<Person> people = new ArrayList<Person>();
-        people.addAll(Person.people);
-        for (Person person : people) {
-            System.out.println(person.getUsername());
+        private void showProducts () {
+            for (Product product : Product.allProducts) {
+                System.out.println(product.getID());
+            }
         }
-    }
 
-    private void viewPerson(String username) {
-        if (!PersonController.usernameTypeErr(username)) {
-            System.out.println("username type incorrect");
-            return;
-        } else if ((person = Person.getPersonByUsername(username)) == null) {
-            System.out.println("there is no user with this username exist");
-            return;
-        }
-        person.toString();
-    }
+        private void removeProduct ( int ID){
 
-    private void deleteUser(String username) {
-        if (!PersonController.usernameTypeErr(username)) {
-            System.out.println("username type incorrect");
-            return;
-        } else if ((person = Person.getPersonByUsername(username)) == null) {
-            System.out.println("there is no user with this username exist");
-            return;
         }
-        Person.deleteUser(username);
-        System.out.println("user deleted successfully");
-    }
 
-    private void createManager() {
-        System.out.println("name: ");
-        String name = Menu.scanner.nextLine();
-        if (!PersonController.nameTypeErr(name)) {
-            System.out.println("name type incorrect");
-            return;
-        }
-        System.out.println("family: ");
-        String family = Menu.scanner.nextLine();
-        System.out.println("username: ");
-        String username = Menu.scanner.nextLine();
-        if (!PersonController.nameTypeErr(username)) {
-            System.out.println("username type incorrect");
-            return;
-        }
-        System.out.println("password: ");
-        String password = Menu.scanner.nextLine();
-        if (!PersonController.checkLengthOfPassWord(password)) {
-            System.out.println("password type incorrect");
-            return;
-        }
-        System.out.println("phone: ");
-        String phone = Menu.scanner.nextLine();
-        if (!PersonController.phoneTypeErr(phone)) {
-            System.out.println("phone type incorrect");
-            return;
-        }
-        System.out.println("email: ");
-        String email = Menu.scanner.nextLine();
-        if (!PersonController.emailTypeErr(email)) {
-            System.out.println("email type incorrect");
-            return;
-        }
-        manager = new Manager(name, family, username, password, phone, email);
-        System.out.println("manager created successfully");
-    }
+        private void createDiscount () {
 
-    private void showProducts() {
-        for (Product product : Product.allProducts) {
-            System.out.println(product.getID());
+        }
+
+        private void showDiscounts () {
+
+        }
+
+        private void viewDiscount ( int id){
+
+        }
+
+        private void editDiscount ( int id){
+
+        }
+
+        private void removeDiscount ( int id){
+
+        }
+
+
+        private static Matcher getMatcher (String input, String regex){
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(input);
+            return matcher;
         }
     }
-
-    private void removeProduct(int ID) {
-
-    }
-
-    private void createDiscount() {
-
-    }
-
-    private void showDiscounts() {
-
-    }
-
-    private void viewDiscount(int id) {
-
-    }
-
-    private void editDiscount(int id) {
-
-    }
-
-    private void removeDiscount(int id) {
-
-    }
-
-
-    private static Matcher getMatcher(String input, String regex) {
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(input);
-        return matcher;
-    }
-}
