72300130fbe1d6d93b9b199325c1bb4155574909
mmdrez4 <mohammadreza.dn80@gmail.com>
2020-05-18 05:14:56 +0430

    test err methods for RegisterProcess class

--- src/test/java/Controller/RegisterProcessTest.java
+++ src/test/java/Controller/RegisterProcessTest.java
@@ -9,10 +9,10 @@ public class RegisterProcessTest {
 
     @Test
     public void createAccountForSeller() {
-        RegisterProcess.createAccountForSeller("Chandler", "Bing", "ch12",
+        Seller.allSellers.add(RegisterProcess.createAccountForSeller("Chandler", "Bing", "ch12",
                 "1234567", "0918765268", "bing@gmail.com",
-                "born in 1982", "Google");
-        assertEquals(true, Seller.isSellerWithThisUsernameExist("Chandler"));
+                "born in 1982", "Google"));
+        assertEquals(true, Seller.isSellerWithThisUsernameExist("ch12"));
     }
 
     @Test
@@ -29,26 +29,38 @@ public class RegisterProcessTest {
 
     @Test
     public void passwordTypeErr() {
+        boolean actualValidation = RegisterProcess.passwordTypeErr("12jdla$%");
+        assertEquals(false, actualValidation);
     }
 
     @Test
     public void checkPasswordUseNumberAndAlphabet() {
+        boolean actualValidation = RegisterProcess.checkPasswordUseNumberAndAlphabet("hello1234");
+        assertEquals(true, actualValidation);
     }
 
     @Test
     public void checkLengthOfPassWord() {
+        boolean actualValidation = RegisterProcess.checkLengthOfPassWord("1234567");
+        assertEquals(false, actualValidation);
     }
 
     @Test
     public void usernameTypeErr() {
+        boolean actualValidation = RegisterProcess.usernameTypeErr("mohammad_dn");
+        assertEquals(true,actualValidation);
     }
 
     @Test
     public void emailTypeErr() {
+        boolean actualValidation = RegisterProcess.emailTypeErr("joey@gmail.com");
+        assertEquals(true,actualValidation);
     }
 
     @Test
     public void phoneTypeErr() {
+        boolean actualValidation = RegisterProcess.phoneTypeErr("78789dsd7s8");
+        assertEquals(false,actualValidation);
     }
 
     @Test
