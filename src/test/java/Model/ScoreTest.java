128018f35aaaf6f57c0c3fc71a3de36799aaf4a5
mmdrez4 <mohammadreza.dn80@gmail.com>
2020-05-18 16:47:38 +0430

    write test for getProduct method in ScoreTest class

--- src/test/java/Model/ScoreTest.java
+++ src/test/java/Model/ScoreTest.java
@@ -5,9 +5,17 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ScoreTest {
+    Buyer exampleBuyer = new Buyer("Joey", "Tribbiani", "Joe_17",
+            "1234", "0912679863", "joey@yahoo.com", 1500, null);
+    Category diaryCategory = new Category("dairy", null);
+    Product productSample = new Product("milk", "good for health", 5.5
+            , "CONFIRMED", diaryCategory, true);
+    Score scoreSample = new Score(exampleBuyer, 5, productSample);
 
     @Test
     public void getProduct() {
+        Product actualProduct = scoreSample.getProduct();
+        assertEquals(productSample, actualProduct);
     }
 
     @Test
