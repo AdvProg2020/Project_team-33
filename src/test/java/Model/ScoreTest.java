5dd9390188bbccd05e042ee9f2bc88510636b373
mmdrez4 <mohammadreza.dn80@gmail.com>
2020-05-18 16:38:52 +0430

    create ScoreTest class

---
+++ src/test/java/Model/ScoreTest.java
@@ -0,0 +1,28 @@
+package Model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ScoreTest {
+
+    @Test
+    public void getProduct() {
+    }
+
+    @Test
+    public void setScore() {
+    }
+
+    @Test
+    public void getPersonWhoGiveScore() {
+    }
+
+    @Test
+    public void getScore() {
+    }
+
+    @Test
+    public void isPersonScoredBefore() {
+    }
+}
