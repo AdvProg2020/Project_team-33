bbe7804af3abb9463ee96827683c0940c1a0818c
mmdrez4 <mohammadreza.dn80@gmail.com>
2020-05-18 20:27:42 +0430

    create test for Comment class

---
+++ src/test/java/Model/CommentTest.java
@@ -0,0 +1,24 @@
+package Model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CommentTest {
+
+    @Test
+    public void getProduct() {
+    }
+
+    @Test
+    public void isPersonBuyProduct() {
+    }
+
+    @Test
+    public void getState() {
+    }
+
+    @Test
+    public void getPersonWhoCommented() {
+    }
+}
