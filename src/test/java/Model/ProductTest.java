b497743278547b03dcb259f4c933585d0a611014
mmdrez4 <mohammadreza.dn80@gmail.com>
2020-05-18 13:51:12 +0430

    create ProductTest class

---
+++ src/test/java/Model/ProductTest.java
@@ -0,0 +1,112 @@
+package Model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProductTest {
+
+    @Test
+    public void getProductID() {
+    }
+
+    @Test
+    public void changeCategory() {
+    }
+
+    @Test
+    public void addSeller() {
+    }
+
+    @Test
+    public void removeSeller() {
+    }
+
+    @Test
+    public void changeDescription() {
+    }
+
+    @Test
+    public void changePrice() {
+    }
+
+    @Test
+    public void getPrice() {
+    }
+
+    @Test
+    public void isProductWithThisNameExist() {
+    }
+
+    @Test
+    public void getProductWithName() {
+    }
+
+    @Test
+    public void getProductWithID() {
+    }
+
+    @Test
+    public void getBuildingTime() {
+    }
+
+    @Test
+    public void getID() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void getProductCategory() {
+    }
+
+    @Test
+    public void getProductState() {
+    }
+
+    @Test
+    public void getIsProductAvailable() {
+    }
+
+    @Test
+    public void getAllSeller() {
+    }
+
+    @Test
+    public void getAllComments() {
+    }
+
+    @Test
+    public void getAllScores() {
+    }
+
+    @Test
+    public void addComment() {
+    }
+
+    @Test
+    public void addScore() {
+    }
+
+    @Test
+    public void addBuyer() {
+    }
+
+    @Test
+    public void getBuyers() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+
+    @Test
+    public void isProductExistInCategory() {
+    }
+}
