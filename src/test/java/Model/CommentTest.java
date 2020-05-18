a7bec821a7ed974853236283a81bd0e966049ca9
mmdrez4 <mohammadreza.dn80@gmail.com>
2020-05-18 20:42:46 +0430

    write getProduct & isPersonBuyProduct methods in CommentTest class

--- src/test/java/Model/CommentTest.java
+++ src/test/java/Model/CommentTest.java
@@ -5,13 +5,21 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CommentTest {
+    NormalPerson samplePerson = new NormalPerson();
+    Product productSample = new Product("milk", "good for health", 5.5
+            , "CONFIRMED", null, true);
+    Comment comment = new Comment(samplePerson, productSample, false);
 
     @Test
     public void getProduct() {
+        Product actualProduct = comment.getProduct();
+        assertEquals(productSample, actualProduct);
     }
 
     @Test
     public void isPersonBuyProduct() {
+        boolean actualResult = comment.isPersonBuyProduct();
+        assertEquals(false,actualResult);
     }
 
     @Test
