7747496fa462244abf520e2c4578e6b378d33de7
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-05-19 06:38:44 +0430

    Create RequestAddSeller class

--- src/main/java/Model/RequestAddSeller.java
+++ src/main/java/Model/RequestAddSeller.java
@@ -4,21 +4,60 @@ import java.util.ArrayList;
 
 public class RequestAddSeller extends Request {
 
-    private Seller seller;
-    private Product product;
-    public static ArrayList<RequestAddSeller> allAddSellerRequest = new ArrayList<RequestAddSeller>();
+    private String username;
+    private String password;
+    private String phone;
+    private String name;
+    private String family;
+    private String email;
+    private String company;
+    private String description;
+    public static ArrayList<RequestAddSeller> allAddSellerRequest = new ArrayList<>();
 
-    public RequestAddSeller(Seller seller, Product product, Auction auction) {
-        super(seller, product, auction);
-        this.seller = seller;
-        this.product = product;
+    public RequestAddSeller(String username, String password, String phone, String name, String family,
+                            String email, String company, String description) {
+        super(null, null, null);
+        this.username = username;
+        this.password = password;
+        this.phone = phone;
+        this.name = name;
+        this.family = family;
+        this.email = email;
+        this.company = company;
+        this.description = description;
+        allAddSellerRequest.add(this);
     }
 
-    public Seller getSeller() {
-        return seller;
+    public String getPassword() {
+        return password;
     }
 
-    public Product getProduct() {
-        return product;
+    public String getCompany() {
+        return company;
     }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
