57acceec669f52813b712003cb69dff2b56b19e2
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-06-14 02:06:35 +0430

    comment some methods

--- src/main/java/View/PurchaseMenu.java
+++ src/main/java/View/PurchaseMenu.java
@@ -5,67 +5,67 @@ import java.util.regex.Pattern;
 
 public class PurchaseMenu {
 
-    public static void receiveInformation() {
-        System.out.println("address:");
-        System.out.println("city:");
-        if (getMatcher("[A-Z]+", Menu.scanner.nextLine()).find()) {
-            System.out.println("street:");
-            if (getMatcher("[A-Z]+", Menu.scanner.nextLine()).find()) {
-                System.out.println("alley:");
-                if (getMatcher("[A-Z]+", Menu.scanner.nextLine()).find()) {
-                    System.out.println("plaque:");
-                    if (getMatcher("\\d+", Menu.scanner.nextLine()).find()) {
-                        System.out.println("phone number:");
-                        if (getMatcher("09\\d{9}", Menu.scanner.nextLine()).find()) {
-                            System.out.println("postal code:");
-                            if (getMatcher("\\d+", Menu.scanner.nextLine()).find()) {
-                                getDiscountCode();
-                            } else {
-                                System.out.println("wrong postal code");
-                            }
-                        } else {
-                            System.out.println("wrong phone number");
-                        }
-
-                    } else {
-                        System.out.println("wrong plaque");
-                    }
-                } else {
-                    System.out.println("wrong alley");
-                }
-            } else {
-                System.out.println("wrong street");
-            }
-        } else {
-            System.out.println("wrong city");
-        }
-    }
-
-    private static void getDiscountCode() {
-        System.out.println("do you have discount code?");
-        while (true) {
-            if (Menu.scanner.nextLine().equalsIgnoreCase("yes")) {
-                System.out.println("write your code");
-                if (getMatcher("\\d+", Menu.scanner.nextLine()).find()) {
-
-                } else {
-                    System.out.println("you can only write numbers");
-                }
-            } else if (Menu.scanner.nextLine().equalsIgnoreCase("no")) {
-                payment();
-                return;
-            } else {
-                System.out.println("invalid answer");
-            }
-        }
-    }
-
-    private static void payment() {
-        System.out.println("");
-    }
-
-    private static Matcher getMatcher(String regex, String input) {
-        Pattern pattern = Pattern.compile(regex);
-        return pattern.matcher(input);
-    }
+//    public static void receiveInformation() {
+//        System.out.println("address:");
+//        System.out.println("city:");
+//        if (getMatcher("[A-Z]+", Menu.scanner.nextLine()).find()) {
+//            System.out.println("street:");
+//            if (getMatcher("[A-Z]+", Menu.scanner.nextLine()).find()) {
+//                System.out.println("alley:");
+//                if (getMatcher("[A-Z]+", Menu.scanner.nextLine()).find()) {
+//                    System.out.println("plaque:");
+//                    if (getMatcher("\\d+", Menu.scanner.nextLine()).find()) {
+//                        System.out.println("phone number:");
+//                        if (getMatcher("09\\d{9}", Menu.scanner.nextLine()).find()) {
+//                            System.out.println("postal code:");
+//                            if (getMatcher("\\d+", Menu.scanner.nextLine()).find()) {
+//                                getDiscountCode();
+//                            } else {
+//                                System.out.println("wrong postal code");
+//                            }
+//                        } else {
+//                            System.out.println("wrong phone number");
+//                        }
+//
+//                    } else {
+//                        System.out.println("wrong plaque");
+//                    }
+//                } else {
+//                    System.out.println("wrong alley");
+//                }
+//            } else {
+//                System.out.println("wrong street");
+//            }
+//        } else {
+//            System.out.println("wrong city");
+//        }
+//    }
+//
+//    private static void getDiscountCode() {
+//        System.out.println("do you have discount code?");
+//        while (true) {
+//            if (Menu.scanner.nextLine().equalsIgnoreCase("yes")) {
+//                System.out.println("write your code");
+//                if (getMatcher("\\d+", Menu.scanner.nextLine()).find()) {
+//
+//                } else {
+//                    System.out.println("you can only write numbers");
+//                }
+//            } else if (Menu.scanner.nextLine().equalsIgnoreCase("no")) {
+//                payment();
+//                return;
+//            } else {
+//                System.out.println("invalid answer");
+//            }
+//        }
+//    }
+//
+//    private static void payment() {
+//        System.out.println("");
+//    }
+//
+//    private static Matcher getMatcher(String regex, String input) {
+//        Pattern pattern = Pattern.compile(regex);
+//        return pattern.matcher(input);
+//    }
 }
