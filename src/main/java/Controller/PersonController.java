528579afbb8b1f95c2dd306d925e7779233ab578
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-06-14 02:00:34 +0430

    comment some methods

--- src/main/java/Controller/PersonController.java
+++ src/main/java/Controller/PersonController.java
@@ -23,6 +23,18 @@ public class PersonController {
         return (matcher = getMatcher(email, "^\\S+@\\S+\\.com$")).find();
     }
 
+    public static boolean checkLengthOfPassWord(String password) {
+        return password.length() < 6;
+    }
+
+    public static boolean phoneTypeErr(String phone) {
+        return (matcher = getMatcher(phone, "^09\\d{9}$")).find();
+    }
+
+    public static boolean existUsername(String username) {
+        return Person.isAccountWithThisUsernameExist(username);
+    }
+
     public static void editPersonalInfo(Person person, String field, String newChange) {
         if (field.equalsIgnoreCase("password")) {
             person.changePassword(newChange);
@@ -55,22 +67,9 @@ public class PersonController {
         return seller.getLogs();
     }
 
-    public static boolean phoneTypeErr(String phone) {
-        return (matcher = getMatcher(phone, "^09\\d{9}$")).find();
-    }
-
-    public static boolean existUsername(String username) {
-        return Person.isAccountWithThisUsernameExist(username);
-    }
-
-    public static boolean checkLengthOfPassWord(String password) {
-        return password.length() < 6;
-    }
 
     private static Matcher getMatcher(String input, String regex) {
         Pattern pattern = Pattern.compile(regex);
         return pattern.matcher(input);
     }
-
-
 }
