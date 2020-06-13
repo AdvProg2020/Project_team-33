b5bb2fd39cd36171e1165240db1ee6f69a497ef8
mmdrez4 <mohammadreza.dn80@gmail.com>
2020-06-13 20:45:42 +0430

    add addComment method to ProductMenuController.java

--- src/main/java/Controller/ProductMenuController.java
+++ src/main/java/Controller/ProductMenuController.java
@@ -4,12 +4,17 @@ import Model.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -92,11 +97,15 @@ public class ProductMenuController implements Initializable {
     }
 
     public void addToBuyLog(MouseEvent mouseEvent) {
-
+//        buyer.getLog().add();
     }
 
-    public void addComment(MouseEvent mouseEvent) {
-
+    public void addComment(MouseEvent mouseEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("CommentsMenu.fxml"));
+        Stage commentStage = new Stage();
+        commentStage.setTitle("Comments");
+        commentStage.setScene(new Scene(root, 600, 600));
+        commentStage.showAndWait();
     }
 
 }
