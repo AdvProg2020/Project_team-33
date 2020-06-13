d566975c19c7f1995e33167f881399a8022be9ee
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-06-14 02:03:58 +0430

    add login menu view

--- src/main/java/View/LoginMenu.java
+++ src/main/java/View/LoginMenu.java
@@ -1,82 +1,30 @@
 package View;
 
-import Controller.RegisterProcess;
 import Model.Person;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LoginMenu extends Menu {
     public static Person currentPerson;
 
-    public LoginMenu() {
-        super("Login Menu ");
-    }
+    public static void loginProcess(Stage stage, Scene scene) throws IOException {
+        if (LoginMenu.currentPerson == null) {
+            URL url = new File("src/main/java/view/userAreaForUnknownNullPerson.fxml").toURI().toURL();
+            Parent root = FXMLLoader.load(url);
+            scene = new Scene(root, 1280, 660);
+            stage.setScene(scene);
+            stage.show();
+        } else {
 
-    public void help() {
-        System.out.println("Enter your command:");
-        System.out.println("login [username]");
-        System.out.println("Help");
-        System.out.println("Back");
-        System.out.println("Exit");
-        commandProcess();
-    }
-
-    public void commandProcess() {
-        String command;
-        while (true) {
-            command = Menu.scanner.nextLine();
-            Matcher matcher;
-            if ((matcher = getMatcher(command, "(login )(\\S)")).find()) {
-                loginProcess(matcher);
-            } else if (command.equalsIgnoreCase("exit")) {
-                System.exit(1);
-            } else if (command.equalsIgnoreCase("help")) {
-                System.out.println("login [username]");
-                System.out.println("Help");
-                System.out.println("Back");
-                System.out.println("Exit");
-            } else if (command.equalsIgnoreCase("back")) {
-                Menu.show();
-            } else {
-                System.out.println("invalid command");
-            }
         }
-    }
-
-    public void loginProcess(Matcher matcher) {
-        String username = matcher.group(2), password;
-        System.out.println("password:");
-        password = Menu.scanner.nextLine();
-        if (getUsernameOfAccount(username)) {
-            if (RegisterMenu.getPasswordOfAccount(password)) {
-                Person person;
-                if (Person.getPersonByUsername(username) != null) {
-                    if ((person=RegisterProcess.login(username,password))!=null) {
-                        System.out.println("login successfully");
-                        currentPerson = person;
-                    } else {
-                        System.out.println("your password is incorrect");
-                    }
-                } else {
-                    System.out.println("there is no user with this username");
-                }
-            }
-        }
-    }
-
-    private boolean getUsernameOfAccount(String username) {
-        if (!RegisterProcess.usernameTypeErr(username)) {
-            System.out.println("Your username is not Valid.");
-            System.out.println("You can use only numbers or alphabet in your username");
-            return false;
-        }
-        return true;
-    }
-
-    private static Matcher getMatcher(String input, String regex) {
-        Pattern pattern = Pattern.compile(regex);
-        return pattern.matcher(input);
     }
 
 }
