f2469ff9222cc3ca1ec3fdac8258691e64080d3f
mmdrez4 <mohammadreza.dn80@gmail.com>
2020-06-14 12:27:09 +0430

    update table view and add id for columns

--- src/main/java/Controller/CommentsMenuController.java
+++ src/main/java/Controller/CommentsMenuController.java
@@ -1,5 +1,32 @@
 package Controller;
 
+import Model.Buyer;
+import Model.Comment;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+import java.awt.*;
+
 public class CommentsMenuController {
 
+    public TextField comment;
+
+    @FXML
+    private TableView<Comment> tableView;
+
+    @FXML
+    private TableColumn<Comment, String> nameColumn;
+
+    @FXML
+    private TableColumn<Comment, String> commentColumn;
+
+    @FXML
+    private TableColumn<Comment, Image> hasBeenBought;
+
+    public void addComment(MouseEvent mouseEvent) {
+
+    }
 }
