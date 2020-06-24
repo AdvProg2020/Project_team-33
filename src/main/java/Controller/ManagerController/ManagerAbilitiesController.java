c9ce0fae9997bb4b83dbfd8ffc4bab74834bf6a5
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-06-24 13:49:02 +0430

    add manager controller class

---
+++ src/main/java/Controller/ManagerController/ManagerAbilitiesController.java
@@ -0,0 +1,26 @@
+package Controller.ManagerController;
+
+import Model.Users.Person;
+
+import java.util.ArrayList;
+
+public class ManagerAbilitiesController {
+
+    public static void editPersonalInfo(Person person, String field, String newChange) {
+        if (field.equalsIgnoreCase("password")) {
+            person.srtPassword(newChange);
+        } else if (field.equalsIgnoreCase("name")) {
+            person.setName(newChange);
+        } else if (field.equalsIgnoreCase("family")) {
+            person.setFamily(newChange);
+        } else if (field.equalsIgnoreCase("phone")) {
+            person.setPhone(newChange);
+        } else if (field.equalsIgnoreCase("email")) {
+            person.setEmail(newChange);
+        }
+    }
+
+    public static ArrayList<Person> getAllMembers() {
+        return null;
+    }
+}
