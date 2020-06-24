1924a911b6bf4cb3c65b7c4749c1bd224058ade4
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-06-24 09:12:14 +0430

    complete LoginMenu.java

--- src/main/java/View/LoginAndRegister/LoginMenu.java
+++ src/main/java/View/LoginAndRegister/LoginMenu.java
@@ -2,6 +2,7 @@ package View.LoginAndRegister;
 
 import Model.Users.Person;
 import View.Menu;
+import View.Menus;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,19 +12,23 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
-public class LoginMenu extends Menu {
+public class LoginMenu extends Menu implements Menus {
     public static Person currentPerson;
 
-    public static void loginProcess(Stage stage, Scene scene) throws IOException {
-        if (LoginMenu.currentPerson == null) {
-            URL url = new File("src/main/java/view/userAreaForUnknownNullPerson.fxml").toURI().toURL();
-            Parent root = FXMLLoader.load(url);
-            scene = new Scene(root, 1280, 660);
-            stage.setScene(scene);
-            stage.show();
-        } else {
-
-        }
+    public void loginProcess() throws IOException {
+        URL url = new File("src/main/java/view/LoginAndRegister/userAreaForUnknownNullPerson.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+        Scene scene = new Scene(root, 1280, 660);
+        Menu.stage.setScene(scene);
+        Menu.stage.show();
     }
 
+    @Override
+    public void show() {
+        try {
+            loginProcess();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
