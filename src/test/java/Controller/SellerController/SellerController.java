7ead3c1ba2de25d45248ee2cdfe82ebe0260f4eb
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-06-25 02:32:29 +0430

    write test for seller controller

---
+++ src/test/java/Controller/SellerController/SellerController.java
@@ -0,0 +1,17 @@
+package Controller.SellerController;
+
+import Controller.ManagerController.ManagerAbilitiesController;
+import Model.Users.Manager;
+import Model.Users.Seller;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SellerController {
+    @Test
+    public void editPersonalInfoForManager() {
+        Seller seller = new Seller("amk_amir", "AmirMahdi", "Kousheshi", "09912310335", "amk_amir82@yahoo.com", "Appleid1234321","google");
+        SellerAbilitiesController.editPersonalInfo(seller, "company", "Microsoft");
+        assertEquals("Microsoft",seller.getCompany());
+    }
+}
