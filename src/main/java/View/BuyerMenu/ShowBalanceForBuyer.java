092a6d0d7bcea94a9d29805aad770b5e54d51adf
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-06-25 00:03:30 +0430

    edit some packages

--- src/main/java/View/BuyerMenu/ShowBalanceForBuyer.java
+++ src/main/java/View/BuyerMenu/ShowBalanceForBuyer.java
@@ -12,31 +12,31 @@ import javafx.stage.Stage;
 
 public class ShowBalanceForBuyer {
 
-    public static void show() {
-        Pane parent = new Pane();
-        Label balance = new Label("Balance");
-        balance.setLayoutX(600);
-        balance.setLayoutY(200);
-        balance.setFont(new Font(25));
-        parent.getChildren().add(balance);
-
-        Button increase = new Button("Increase");
-        increase.setLayoutX(580);
-        increase.setLayoutY(350);
-        increase.setCursor(Cursor.HAND);
-        parent.getChildren().add(increase);
-
-        Button back = new Button("Back");
-        back.setLayoutX(650);
-        back.setLayoutY(350);
-        back.setCursor(Cursor.HAND);
-        parent.getChildren().add(back);
-
-
-        Scene scene = new Scene(parent, 1280, 660);
-        Menu.stage.setScene(scene);
-        Menu.stage.show();
-    }
+//    public static void show() {
+//        Pane parent = new Pane();
+//        Label balance = new Label("Balance");
+//        balance.setLayoutX(600);
+//        balance.setLayoutY(200);
+//        balance.setFont(new Font(25));
+//        parent.getChildren().add(balance);
+//
+//        Button increase = new Button("Increase");
+//        increase.setLayoutX(580);
+//        increase.setLayoutY(350);
+//        increase.setCursor(Cursor.HAND);
+//        parent.getChildren().add(increase);
+//
+//        Button back = new Button("Back");
+//        back.setLayoutX(650);
+//        back.setLayoutY(350);
+//        back.setCursor(Cursor.HAND);
+//        parent.getChildren().add(back);
+//
+//
+//        Scene scene = new Scene(parent, 1280, 660);
+//        Menu.stage.setScene(scene);
+//        Menu.stage.show();
+//    }
 
 
 //    @Override
