9e5f366a6b09b26aef99e3208193c77be7803087
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-06-24 09:49:25 +0430

    edit program for run

--- src/main/java/Model/Users/Manager.java
+++ src/main/java/Model/Users/Manager.java
@@ -1,9 +1,12 @@
 package Model.Users;
 
+import Database.SaveData;
+
 import java.util.ArrayList;
 
 public class Manager extends Person {
-    public static ArrayList<Manager> allManagers = new ArrayList<Manager>();
+    public static ArrayList<Manager> allManagers = new ArrayList<>();
+    SaveData saveData = new SaveData();
 
     public Manager(String username, String name, String family, String phone,
                    String email, String password) {
