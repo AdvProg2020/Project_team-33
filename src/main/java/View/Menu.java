a049c048c4a801845f3f4be42149e72cb5f810c6
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-06-24 06:00:39 +0430

    edit packages

--- src/main/java/View/Menu.java
+++ src/main/java/View/Menu.java
@@ -1,15 +1,10 @@
 package View;
 
-import Controller.PersonController;
+import View.LoginAndRegister.LoginMenu;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.File;
