8a37ccca527123cee00fe8bfcece7c1125e0ea73
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-06-24 08:51:11 +0430

    complete PersonController.java

--- src/main/java/Controller/PersonController.java
+++ src/main/java/Controller/PersonController.java
@@ -19,6 +19,10 @@ public class PersonController {
         return (matcher = getMatcher(username, "^[A-Za-z0-9_]+$")).find();
     }
 
+    public static boolean phoneTypeErr(String phone) {
+        return (matcher = getMatcher(phone, "^09\\d{9}$")).find();
+    }
+
     public static boolean emailTypeErr(String email) {
         return (matcher = getMatcher(email, "^\\S+@\\S+\\.com$")).find();
     }
@@ -27,48 +31,54 @@ public class PersonController {
         return password.length() < 6;
     }
 
-    public static boolean phoneTypeErr(String phone) {
-        return (matcher = getMatcher(phone, "^09\\d{9}$")).find();
-    }
-
     public static boolean existUsername(String username) {
         return Person.isAccountWithThisUsernameExist(username);
-    }
-
-    public static void editPersonalInfo(Person person, String field, String newChange) {
-        if (field.equalsIgnoreCase("password")) {
-            person.changePassword(newChange);
-        } else if (field.equalsIgnoreCase("name")) {
-            person.changeName(newChange);
-        } else if (field.equalsIgnoreCase("family")) {
-            person.changeFamily(newChange);
-        } else if (field.equalsIgnoreCase("phone")) {
-            person.changePhone(newChange);
-        } else if (field.equalsIgnoreCase("email")) {
-            person.changeEmail(newChange);
-        }
-    }
-
-    public static ArrayList<Person> getPeople() {
-        return Person.getPeople();
-    }
-
-    public static ArrayList<Product> getProducts() {
-        ArrayList<Product> products = new ArrayList<>();
-        products.addAll(Product.allProducts);
-        return products;
-    }
-
-    public static void deleteUsers(String username) {
-        Person.deleteUser(username);
-    }
-
-    public static ArrayList<SellLog> sellerSellLogs(Seller seller) {
-        return seller.getLogs();
     }
 
     private static Matcher getMatcher(String input, String regex) {
         Pattern pattern = Pattern.compile(regex);
         return pattern.matcher(input);
     }
+
+
+
+
+
+
+
+
+
+
+//    public static void editPersonalInfo(Person person, String field, String newChange) {
+//        if (field.equalsIgnoreCase("password")) {
+//            person.srtPassword(newChange);
+//        } else if (field.equalsIgnoreCase("name")) {
+//            person.setName(newChange);
+//        } else if (field.equalsIgnoreCase("family")) {
+//            person.setFamily(newChange);
+//        } else if (field.equalsIgnoreCase("phone")) {
+//            person.setPhone(newChange);
+//        } else if (field.equalsIgnoreCase("email")) {
+//            person.setEmail(newChange);
+//        }
+//    }
+
+//    public static ArrayList<Person> getPeople() {
+//        return Person.getPeople();
+//    }
+
+//    public static ArrayList<Product> getProducts() {
+//        ArrayList<Product> products = new ArrayList<>();
+//        products.addAll(Product.allProducts);
+//        return products;
+//    }
+
+//    public static void deleteUsers(String username) {
+//        Person.deleteUser(username);
+//    }
+
+//    public static ArrayList<SellLog> sellerSellLogs(Seller seller) {
+//        return seller.getLogs();
+//    }
+
 }
