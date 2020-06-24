72ea3fba5d58ff53fe232668f08ccaeddcf82799
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-06-24 21:56:43 +0430

    comment class for phase3

--- src/main/java/View/SellerMenu/ShowSellLogsForSeller.java
+++ src/main/java/View/SellerMenu/ShowSellLogsForSeller.java
@@ -8,58 +8,58 @@ import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-public class ShowSellLogsForSeller extends Application {
+public class ShowSellLogsForSeller {
 
-    public static void show() {
-        Pane parent = new Pane();
-        parent.setStyle("-fx-background-color: #858585");
-        Label productName = new Label("Serial");
-        productName.setLayoutX(50);
-        productName.setLayoutY(50);
-        productName.setFont(new Font(25));
-        parent.getChildren().add(productName);
+//    public static void show() {
+//        Pane parent = new Pane();
+//        parent.setStyle("-fx-background-color: #858585");
+//        Label productName = new Label("Serial");
+//        productName.setLayoutX(50);
+//        productName.setLayoutY(50);
+//        productName.setFont(new Font(25));
+//        parent.getChildren().add(productName);
+//
+//        Label productPrice = new Label("Price");
+//        productPrice.setLayoutX(350);
+//        productPrice.setLayoutY(50);
+//        productPrice.setFont(new Font(25));
+//        parent.getChildren().add(productPrice);
+//
+//        Label numberOfProduct = new Label("Date");
+//        numberOfProduct.setLayoutX(550);
+//        numberOfProduct.setLayoutY(50);
+//        numberOfProduct.setFont(new Font(25));
+//        parent.getChildren().add(numberOfProduct);
+//
+//        Label increaseOrDecrease = new Label("Off");
+//        increaseOrDecrease.setLayoutX(750);
+//        increaseOrDecrease.setLayoutY(50);
+//        increaseOrDecrease.setFont(new Font(25));
+//        parent.getChildren().add(increaseOrDecrease);
+//
+//        Label finalPrice = new Label("Final Price");
+//        finalPrice.setLayoutX(950);
+//        finalPrice.setLayoutY(50);
+//        finalPrice.setFont(new Font(25));
+//        parent.getChildren().add(finalPrice);
+//
+//        Label delivery = new Label("Delivery");
+//        delivery.setLayoutX(1150);
+//        delivery.setLayoutY(50);
+//        delivery.setFont(new Font(25));
+//        parent.getChildren().add(delivery);
+//
+//        Scene scene = new Scene(parent, 1280, 660);
+//
+//        Menu.stage.setScene(scene);
+//
+//        Menu.stage.show();
+//    }
 
-        Label productPrice = new Label("Price");
-        productPrice.setLayoutX(350);
-        productPrice.setLayoutY(50);
-        productPrice.setFont(new Font(25));
-        parent.getChildren().add(productPrice);
 
-        Label numberOfProduct = new Label("Date");
-        numberOfProduct.setLayoutX(550);
-        numberOfProduct.setLayoutY(50);
-        numberOfProduct.setFont(new Font(25));
-        parent.getChildren().add(numberOfProduct);
-
-        Label increaseOrDecrease = new Label("Off");
-        increaseOrDecrease.setLayoutX(750);
-        increaseOrDecrease.setLayoutY(50);
-        increaseOrDecrease.setFont(new Font(25));
-        parent.getChildren().add(increaseOrDecrease);
-
-        Label finalPrice = new Label("Final Price");
-        finalPrice.setLayoutX(950);
-        finalPrice.setLayoutY(50);
-        finalPrice.setFont(new Font(25));
-        parent.getChildren().add(finalPrice);
-
-        Label delivery = new Label("Delivery");
-        delivery.setLayoutX(1150);
-        delivery.setLayoutY(50);
-        delivery.setFont(new Font(25));
-        parent.getChildren().add(delivery);
-
-        Scene scene = new Scene(parent, 1280, 660);
-
-        Menu.stage.setScene(scene);
-
-        Menu.stage.show();
-    }
-
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        show();
-    }
+//    @Override
+//    public void start(Stage stage) throws Exception {
+//        show();
+//    }
 }
 
