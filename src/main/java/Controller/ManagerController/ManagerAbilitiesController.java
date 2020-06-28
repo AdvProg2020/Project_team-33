9df66374e300135ebfa2f59b9d8b40bc1d4f4a1f
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-06-29 00:04:40 +0430

    complete manager controller

--- src/main/java/Controller/ManagerController/ManagerAbilitiesController.java
+++ src/main/java/Controller/ManagerController/ManagerAbilitiesController.java
@@ -1,15 +1,16 @@
 package Controller.ManagerController;
 
+import Model.Discount;
 import Model.Users.Person;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
 
 public class ManagerAbilitiesController {
-
 
 
     public static void editPersonalInfo(Person person, String field, String newChange) {
@@ -34,6 +35,31 @@ public class ManagerAbilitiesController {
         Person.deleteUser(person);
     }
 
+    public static ArrayList<Discount> getAllDiscounts() {
+        return Discount.getAllDiscounts();
+    }
+
+    public static void deleteDiscount(Discount discount) {
+        Discount.deleteDiscount(discount);
+    }
+
+    public static void editDiscount(Discount discount, String field, String newChange) {
+        if (field.equalsIgnoreCase("code")) {
+            discount.setCode(newChange);
+        } else if (field.equalsIgnoreCase("start time")) {
+            String[] input = newChange.split(":");
+            LocalTime localTime = LocalTime.of(Integer.parseInt(input[0]), Integer.parseInt(input[1]));
+            discount.setStartTime(localTime);
+        } else if (field.equalsIgnoreCase("end time")) {
+            String[] input = newChange.split(":");
+            LocalTime localTime = LocalTime.of(Integer.parseInt(input[0]), Integer.parseInt(input[1]));
+            discount.setEndTime(localTime);
+        } else if (field.equalsIgnoreCase("percent")) {
+            discount.setDiscountPercent(Integer.parseInt(newChange));
+        } else if (field.equalsIgnoreCase("max discount")) {
+            discount.setMaxDiscount(Long.parseLong(newChange));
+        }
+    }
 
 
 }
