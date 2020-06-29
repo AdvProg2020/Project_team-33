990c4a985c6672f459bb0c192dc033de53dc6a11
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-06-29 19:50:22 +0430

    product class almost complete

--- src/main/java/Model/Product.java
+++ src/main/java/Model/Product.java
@@ -1,200 +1,104 @@
 package Model;
 
 import Model.Category.Category;
+import Model.Category.SubCategory;
 import Model.Users.Buyer;
 import Model.Users.Seller;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-public class Product{
-    public static ArrayList<Product> allProducts = new ArrayList<>();
-    private ArrayList<SellerOfProduct> allSeller = new ArrayList<>();
-    private String description, name;
-    private int productID;
+public class Product {
+    private String productID;
+    private String name;
+    private String condition;
+    private String company;
+    private long money;
+    private Seller seller;
+    private String inventoryStatus;
+    private Category category;
+    private SubCategory subCategory;
+    private String description;
+    private double averageScore;
+    private int numberOfProducts = 0;
+    private String requestCondition;
     private ArrayList<Score> allScores = new ArrayList<>();
     private ArrayList<Comment> allComments = new ArrayList<>();
-    private ArrayList<Buyer> buyers = new ArrayList<>();
-    public static int numberOfProductsFromBegin = 0;
-    private productState state;
-    private Category productCategory;
-    private boolean isProductAvailable;
-    private int seenNumber;
-    private LocalDate buildingTime;
+    private ArrayList<SellerOfProduct> allSeller = new ArrayList<>();
+    private static ArrayList<Product> allProducts = new ArrayList<>();
 
-    public Product(String name, String description, double price, String state, Category category,
-                   boolean isProductAvailable) {
-        allProducts.add(this);
+    public Product(String productID, String name, String company, long money, Seller seller,
+                   Category category, String description, String requestCondition) {
+        this.productID = productID;
         this.name = name;
+        this.company = company;
+        this.money = money;
+        this.seller = seller;
+        this.category = category;
         this.description = description;
-        this.state = productState.valueOf(state);
-        this.productCategory = category;
-        this.isProductAvailable = isProductAvailable;
-        numberOfProductsFromBegin++;
-        this.productID = numberOfProductsFromBegin;
-        this.seenNumber = 0;
-        this.buildingTime = LocalDate.now();
+        this.numberOfProducts++;
+        this.requestCondition = requestCondition;
+        seller.addProduct(this);
     }
 
-    public int calculateAveragePrice(){
-        int sum = 0;
-        int number = 0;
-        for (SellerOfProduct sellerOfProduct : allSeller) {
-            sum += sellerOfProduct.getPrice();
-            number++;
-        }
-        return sum / number;
-    }
-
-    public static void removeProduct(int Id) {
-        allProducts.remove(getProductWithID(Id));
-    }
-
-    public int getProductID() {
+    public String getProductID() {
         return productID;
     }
 
-    public void changeCategory(Category newCategory) {
-        this.productCategory = newCategory;
-    }
-
-    public void addSeller(Seller seller, double price) {
-        this.allSeller.add(new SellerOfProduct(seller, price));
-    }
-
-    public void removeSeller(Seller seller) {
-        this.allSeller.remove(seller);
-    }
-
-    public void changeDescription(String newDescription) {
-        this.description = newDescription;
-    }
-
-    public static boolean isProductWithThisNameExist(String name) {
-        for (Product eachProduct : allProducts)
-            if (eachProduct.getName().equals(name)) return true;
-        return true;
-    }
-
-    public static Product getProductWithName(String name) {
-        for (Product eachProduct : allProducts)
-            if (eachProduct.getName().equals(name)) return eachProduct;
-        return null;
-    }
-
-    public static Product getProductWithID(int ID) {
-        for (Product eachProduct : allProducts)
-            if (eachProduct.getID() == ID) return eachProduct;
-        return null;
-    }
-
-    public SellerOfProduct getSellerByName(String name) {
-        for (SellerOfProduct seller : allSeller) {
-            if (seller.getSeller().equals(name)) {
-                return seller;
-            }
-        }
-        return null;
-    }
-
-    public LocalDate getBuildingTime() {
-        return this.buildingTime;
-    }
-
-    public int getID() {
-        return this.productID;
-    }
-
     public String getName() {
-        return this.name;
+        return name;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public long getMoney() {
+        return money;
+    }
+
+    public Seller getSeller() {
+        return seller;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public SubCategory getSubCategory() {
+        return subCategory;
     }
 
     public String getDescription() {
-        return this.description;
+        return description;
     }
 
-    public Category getProductCategory() {
-        return this.productCategory;
+    public int getNumberOfProducts() {
+        return numberOfProducts;
     }
 
-    public String getProductState() {
-        switch (this.state) {
-            case BUILDING:
-                return "Product is Building";
-            case EDITING:
-                return "Product is Editing";
-            case CONFIRMED:
-                return "Product is Ready for Buy";
-        }
-        return null;
+    public void setInventoryStatus(String inventoryStatus) {
+        this.inventoryStatus = inventoryStatus;
     }
 
-    public boolean getIsProductAvailable() {
-        return this.isProductAvailable;
+    public double getAverageScore() {
+        return averageScore;
     }
 
-    public boolean isBuyerBoughtThisProduct(Buyer buyer) {
-        return this.getBuyers().contains(buyer);
+    public void setAverageScore(double averageScore) {
+        this.averageScore = averageScore;
     }
 
-    public ArrayList<SellerOfProduct> getAllSeller() {
-        return this.allSeller;
-    }
-
-    public ArrayList<Comment> getAllComments() {
-        return this.allComments;
-    }
-
-    public ArrayList<Score> getAllScores() {
-        return this.allScores;
-    }
-
-    public void addComment(Comment comment) {
-        this.allComments.add(comment);
-    }
-
-    public void addScore(Score score) {
-        this.allScores.add(score);
-    }
-
-    public void addBuyer(Buyer buyer) {
-        this.buyers.add(buyer);
-    }
-
-    public ArrayList<Buyer> getBuyers() {
-        return this.buyers;
-    }
-
-//    public boolean isProductExistInCategory(Category category) {
-//        if (this.getProductCategory() == null) return false;
-//        else if (this.getProductCategory().equals(category)) return true;
-//        Category example = this.getProductCategory();
-//        while (example.isCategoryHasSuper()) {
-//            example = example.getSuperCategory();
-//            if (example.equals(category)) return true;
-//        }
-//        return false;
-//    }
-
-    public double findPriceOfThisSeller(Seller seller) {
-        for (SellerOfProduct example : allSeller)
-            if (seller.equals(example.getSeller())) return example.getPrice();
-        return 0;
-    }
-
-    public double findLeastPrice() {
-        double leastPrice = allSeller.get(0).getPrice();
-        for (SellerOfProduct sellerOfProduct : allSeller) {
-            if (sellerOfProduct.getPrice() < leastPrice) {
-                leastPrice = sellerOfProduct.getPrice();
+    public static boolean isProductExist(String code) {
+        for (Product allProduct : allProducts) {
+            if (allProduct.getProductID().equals(code)) {
+                return true;
             }
         }
-        return leastPrice;
+        return false;
     }
-}
-
-enum productState {
-    BUILDING,
-    EDITING,
-    CONFIRMED
 }
