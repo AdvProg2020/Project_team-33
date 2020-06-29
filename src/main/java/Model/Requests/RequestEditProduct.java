fe73806e720761ffec2a7069a675ffdbf3a9ccb4
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-06-29 12:58:08 +0430

    change request

--- src/main/java/Model/Requests/RequestEditProduct.java
+++ src/main/java/Model/Requests/RequestEditProduct.java
@@ -6,13 +6,13 @@ import Model.Users.Seller;
 
 import java.util.ArrayList;
 
-public class RequestEditProduct extends Request {
+public class RequestEditProduct  {
     private Seller seller;
     private Product product;
     public static ArrayList<RequestEditProduct> allAddProductRequest = new ArrayList<RequestEditProduct>();
 
     public RequestEditProduct(Seller seller, Product product, Auction auction) {
-        super(seller, product, auction);
+//        super(seller, product, auction);
         this.seller = seller;
         this.product = product;
         allAddProductRequest.add(this);
