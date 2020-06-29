220dea83968bc34e32c72775054b97827a3c4c4f
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-06-29 12:56:26 +0430

    add seller request complete

--- src/main/java/Model/Requests/RequestAddAuction.java
+++ src/main/java/Model/Requests/RequestAddAuction.java
@@ -6,13 +6,13 @@ import Model.Users.Seller;
 
 import java.util.ArrayList;
 
-public class RequestAddAuction extends Request {
+public class RequestAddAuction  {
     private Seller seller;
     private Auction auction;
     public static ArrayList<RequestAddAuction> allAddAuctionRequest = new ArrayList<RequestAddAuction>();
 
     public RequestAddAuction(Seller seller, Product product, Auction auction) {
-        super(seller, product, auction);
+//        super(seller, product, auction);
         this.seller = seller;
         this.auction = auction;
         allAddAuctionRequest.add(this);
