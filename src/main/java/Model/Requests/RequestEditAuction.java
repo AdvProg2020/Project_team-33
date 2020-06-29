70be0a32c9dd798cef6bfe37f8d5f38eac740dd5
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-06-29 12:57:58 +0430

    change request

--- src/main/java/Model/Requests/RequestEditAuction.java
+++ src/main/java/Model/Requests/RequestEditAuction.java
@@ -6,14 +6,14 @@ import Model.Users.Seller;
 
 import java.util.ArrayList;
 
-public class RequestEditAuction extends Request {
+public class RequestEditAuction  {
     private Seller seller;
     private Auction auction;
     public static ArrayList<RequestEditAuction> allEditAuctionRequests = new ArrayList<RequestEditAuction>();
 
 
     public RequestEditAuction(Seller seller, Product product, Auction auction) {
-        super(seller, product, auction);
+//        super(seller, product, auction);
         this.seller = seller;
         this.auction = auction;
         allEditAuctionRequests.add(this);
