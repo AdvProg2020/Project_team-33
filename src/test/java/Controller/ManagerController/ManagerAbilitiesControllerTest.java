f9603ebbfe66a31f8fa4336983f5905da5b8490c
mmdrez4 <mohammadreza.dn80@gmail.com>
2020-06-29 16:10:09 +0430

    create getAllDiscountsAndDeleteDiscount method in ManagerAbilitiesControllerTest.java

--- src/test/java/Controller/ManagerController/ManagerAbilitiesControllerTest.java
+++ src/test/java/Controller/ManagerController/ManagerAbilitiesControllerTest.java
@@ -1,17 +1,19 @@
 package Controller.ManagerController;
 
+import Model.Discount;
 import Model.Users.Buyer;
 import Model.Users.Manager;
 import Model.Users.Person;
 import Model.Users.Seller;
 import org.junit.Test;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class ManagerAbilitiesControllerTest {
-    ArrayList<Person> people = new ArrayList<>();
 
     @Test
     public void editPersonalInfoForManager() {
@@ -27,15 +29,37 @@ public class ManagerAbilitiesControllerTest {
         assertEquals("ali.vanaki100@gmail.com", manager.getEmail());
         assertEquals("vanaki", manager.getFamily());
         assertEquals("09107270737", manager.getPhone());
-        people.add(manager);
     }
 
     @Test
     public void getPeopleFromManger() {
         Person.people.clear();
+        ArrayList<Person> people = new ArrayList<>();
         people.add(new Buyer("mohammad", "mohammad", "daviran", "1234", "mr@gmail.com", " 123"));
         people.add(new Seller("mohammad", "mohammad", "daviran", "1234", "mr@gmail.com", " 123", "Apple"));
         assertEquals(people, ManagerAbilitiesController.getAllMembers());
+    }
+
+    @Test
+    public void deleteUser(){
+        Person.people.clear();
+        Buyer buyer = new Buyer("mohammad", "mohammad", "daviran", "1234", "mr@gmail.com", " 123");
+        ManagerAbilitiesController.deleteUser(buyer);
+        boolean actual = Person.people.isEmpty();
+        assertTrue(actual);
+    }
+
+    @Test
+    public void getAllDiscountsAndDeleteDiscount(){
+        ArrayList<Discount> discounts = new ArrayList<>();
+        LocalTime start = LocalTime.now();
+        LocalTime end = start.plusMinutes(10);
+        Discount discount = new Discount("123", start, end, (long)10, 15);
+        discounts.add(discount);
+        assertEquals(discounts, ManagerAbilitiesController.getAllDiscounts());
+        ManagerAbilitiesController.deleteDiscount(discount);
+        boolean actual = ManagerAbilitiesController.getAllDiscounts().isEmpty();
+        assertTrue(actual);
     }
 
     @Test
