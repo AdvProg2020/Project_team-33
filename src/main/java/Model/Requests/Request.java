0fa8343d9dc27bccd930e28912f1f76036cae274
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-06-29 12:56:13 +0430

    add seller request complete

--- src/main/java/Model/Requests/Request.java
+++ src/main/java/Model/Requests/Request.java
@@ -2,26 +2,54 @@ package Model.Requests;
 
 import Model.Auction;
 import Model.Product;
+import Model.Users.Person;
 import Model.Users.Seller;
 
 import java.util.ArrayList;
 
 public class Request {
+    String type;
+    String condition;
+    Person sender;
     public static ArrayList<Request> allRequests = new ArrayList<>();
 
-    public Request(Seller seller, Product product, Auction auction) {
-
+    public Request(String type, String condition, Person sender) {
+        this.type = type;
+        this.condition = condition;
+        this.sender = sender;
+        allRequests.add(this);
     }
 
-    public void addRequest(Request request) {
-
+    public void setCondition(String condition) {
+        this.condition = condition;
+        if (condition.equals("Accept")) {
+            Seller seller = (Seller) this.getSender();
+            seller.setCondition("Accept");
+        } else if (condition.equals("Decline")) {
+            Seller seller = (Seller) this.getSender();
+            seller.setCondition("Decline");
+        }
     }
 
-    public void deleteRequest(Request request) {
-
+    public static ArrayList<Request> getAllRequests() {
+        return allRequests;
     }
 
-    public Request getRequest() {
-        return null;
+    public String getType() {
+        return type;
     }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public Person getSender() {
+        return sender;
+    }
+
+    public static void deleteRequest(Request request) {
+        allRequests.remove(request);
+    }
+
+
 }
