fa90140d8aea27d81a1ef136e3ddb994e74e164f
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-07-01 21:39:07 +0430

    add product

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import Controller.PersonController;
 import Database.SaveData;
 import Model.Category.Category;
 import Model.Discount;
+import Model.Product;
 import Model.Requests.RequestAddSeller;
 import Model.Users.Buyer;
 import Model.Users.Manager;
@@ -31,9 +32,10 @@ public class Main extends Application {
         Manager mainManager = new Manager("amk_amir", "Amir Mahdi", "Kousheshi", "09912310335", "amk_amir82@yahoo.com", "Appleid1234321");
         PersonController.isManagerAccountCreate = true;
         PersonController.mainManager = mainManager;
-        new Buyer("saba_sk", "saba", "keshavarz", "09912310335", "saba@yahoo.com", "sabasasa");
+         new Buyer("saba_sk", "saba", "keshavarz", "09912310335", "saba@yahoo.com", "sabasasa");
         LoginMenu.currentPerson = mainManager;
-        new Seller("amirsalar", "amirsalar", "ansari", "09131789201", "a@a.com", "09131789201", "yes");
+        Seller seller = new Seller("amirsalar", "amirsalar", "ansari", "09131789201", "a@a.com", "09131789201", "yes");
+        Seller seller1 = new Seller("arya", "Arya", "Jalali", "09123456789", "a@a.com", "areare", "Apple");
         LocalTime localTime = LocalTime.of(21, 30);
         LocalTime localTime1 = LocalTime.of(22, 30);
         new Discount("981710", localTime, localTime1, (long) 50_000, 50);
@@ -42,7 +44,12 @@ public class Main extends Application {
         strings.add("Samsung");
         strings.add("Apple");
         strings.add("LG");
-        new Category("Mobiles", null, strings);
+        Category category = new Category("Mobiles", null, strings);
+        new Product("981710", "galaxy S6", "Samsung", 2000000, seller, category, "A good phone", "Unknown");
+        new Product("981711", "Iphone x", "Apple", 2000000, seller1, category, "A good phone", "Unknown");
+        new Product("981712", "Iphone xs", "Apple", 2000000, seller1, category, "A good phone", "Unknown");
+        new Product("981713", "Iphone xs max", "Apple", 2000000, seller1, category, "A good phone", "Unknown");
+
         Menu menu = new Menu();
         Menu.currentMenu = menu;
         Menu.previousMenu = menu;
