51807180aa92d9fbdcc5b818b48d2488a7af2e5e
mmdrez4 <mohammadreza.dn80@gmail.com>
2020-07-04 02:19:56 +0430

    create Supporter.java class

---
+++ src/main/java/Model/Users/Supporter.java
@@ -0,0 +1,10 @@
+package Model.Users;
+
+public class Supporter extends Person{
+
+
+    public Supporter(String username, String name, String family, String phone, String email, String password) {
+        super(username, name, family, phone, email, password);
+    }
+
+}
