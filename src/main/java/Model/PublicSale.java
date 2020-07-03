91791751af7ac073045a4dda183e05b16f730ae9
mmdrez4 <mohammadreza.dn80@gmail.com>
2020-07-04 02:39:10 +0430

    create PublicSale.java class

---
+++ src/main/java/Model/PublicSale.java
@@ -0,0 +1,11 @@
+package Model;
+
+import Model.Users.Seller;
+
+import java.time.LocalTime;
+
+public class PublicSale {
+    private LocalTime localTime;
+    private Seller seller;
+
+}
--- src/main/java/Model/Users/Supporter.java
+++ src/main/java/Model/Users/Supporter.java
@@ -2,7 +2,6 @@ package Model.Users;
 
 public class Supporter extends Person{
 
-
     public Supporter(String username, String name, String family, String phone, String email, String password) {
         super(username, name, family, phone, email, password);
     }
