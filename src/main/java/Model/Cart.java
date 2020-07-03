d72767414cd065fd4d5c129374a3f00eb1c9794c
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-07-03 17:14:26 +0430

    cart

--- src/main/java/Model/Cart.java
+++ src/main/java/Model/Cart.java
@@ -6,18 +6,49 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Cart {
+    private HashMap<Product, Integer> numberOfProductsInPage;
     private ArrayList<Product> productsInCart;
 
     public Cart() {
         this.productsInCart = new ArrayList<>();
+        this.numberOfProductsInPage = new HashMap<>();
     }
 
     public void addProductToCart(Product product) {
         this.productsInCart.add(product);
+        numberOfProductsInPage.put(product, 1);
     }
 
+    public void clear() {
+        this.productsInCart.clear();
+    }
 
-//    public ArrayList<BuyingProduct> getProductsInCart() {
+    public ArrayList<Product> getProductsInCart() {
+        return productsInCart;
+    }
+
+    public void setNumberOfProductsInPage(Product product, int number) {
+        if (number <= 0) {
+            productsInCart.remove(product);
+            numberOfProductsInPage.remove(product);
+        } else {
+            numberOfProductsInPage.replace(product, number);
+        }
+    }
+
+    public int getNumberOfProductsInPage(Product product) {
+        return numberOfProductsInPage.get(product);
+    }
+
+    public double getMoneyForPurchase() {
+        double money = 0;
+        for (Product product : this.productsInCart) {
+            money += product.getMoney();
+        }
+        return money;
+    }
+
+    //    public ArrayList<BuyingProduct> getProductsInCart() {
 //        return productsInCart;
 //    }
 //
