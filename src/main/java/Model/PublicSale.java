91a23b5cabf9aef7e5112048125dd4d5bd7d73ce
mmdrez4 <mohammadreza.dn80@gmail.com>
2020-07-04 02:51:20 +0430

    update PublicSale.java

--- src/main/java/Model/PublicSale.java
+++ src/main/java/Model/PublicSale.java
@@ -1,11 +1,48 @@
 package Model;
 
+import Model.Users.Buyer;
 import Model.Users.Seller;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
 
 public class PublicSale {
-    private LocalTime localTime;
+    private LocalTime endTime;
     private Seller seller;
+    private ArrayList<Buyer> participants;
+    private Buyer winner;
 
+    public PublicSale(Seller seller, LocalTime endTime) {
+        this.endTime = endTime;
+        this.seller = seller;
+        participants= new ArrayList<>();
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public Seller getSeller() {
+        return seller;
+    }
+
+    public void setSeller(Seller seller) {
+        this.seller = seller;
+    }
+
+    public void addParticipant(Buyer buyer){
+        participants.add(buyer);
+    }
+
+    public Buyer getWinner() {
+        return winner;
+    }
+
+    public void setWinner(Buyer winner) {
+        this.winner = winner;
+    }
 }
