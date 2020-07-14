3750071183f0f5efd6e2fecc9f1082ba096ce321
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-07-14 19:12:05 +0430

    complete auction model

--- src/main/java/Model/Auction.java
+++ src/main/java/Model/Auction.java
@@ -1,72 +1,45 @@
 package Model;
 
+import Model.Users.Seller;
+
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class Auction {
-    public static ArrayList<Auction> allAuctions;
-    private Date beginningTime;
-    private Date finishingTime;
+    private Seller seller;
+    private String id;
     private ArrayList<Product> products;
-    private AuctionStatue auctionStatue;
-    private int discount;
+    private String state;
+    private LocalTime start;
+    private LocalTime end;
+    private int discountPercent;
+    public static ArrayList<Auction> allAuctions = new ArrayList<>();
 
-    public Auction(Date beginningTime, Date finishingTime, ArrayList<Product> products, int discount) {
-        this.beginningTime = beginningTime;
-        this.finishingTime = finishingTime;
+
+    public Auction(Seller seller, String id, ArrayList<Product> products, LocalTime start, LocalTime end, int discountPercent) {
+        this.seller = seller;
+        this.id = id;
         this.products = products;
-        this.discount = discount;
+        this.state = "Creating";
+        this.start = start;
+        this.end = end;
+        this.discountPercent = discountPercent;
+        allAuctions.add(this);
     }
 
-    boolean isThisProductExistInAuction(Product product) {
-        for (Product eachProduct : products) {
-            if (eachProduct.equals(product)) {
+    public static boolean isIdExist(String id) {
+        for (Auction allAuction : allAuctions) {
+            if (id.equals(allAuction.getId())) {
                 return true;
             }
         }
         return false;
     }
 
-    public Date getBeginningTime() {
-        return beginningTime;
+    public String getId() {
+        return id;
     }
-
-    public void setBeginningTime(Date beginningTime) {
-        this.beginningTime = beginningTime;
-    }
-
-    public Date getFinishingTime() {
-        return finishingTime;
-    }
-
-    public void setFinishingTime(Date finishingTime) {
-        this.finishingTime = finishingTime;
-    }
-
-    public int getDiscount() {
-        return discount;
-    }
-
-    public void setDiscount(int discount) {
-        this.discount = discount;
-    }
-
-    public void addProduct(Product product) {
-        products.add(product);
-    }
-
-    public void deleteProduct(Product product) {
-        for (Product eachProduct : products)
-            if (eachProduct.equals(product)) this.products.remove(product);
-    }
-
-    public Auction getAuctionById(int id) {
-        if (id > allAuctions.size()) {
-            return null;
-        }
-        return allAuctions.get(id);
-    }
-
 }
 
 enum AuctionStatue {
