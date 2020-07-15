ef5c044a72f16ad281651ed6e6c0d1b7c4af8089
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-07-15 18:53:12 +0430

    complete auction requests

--- src/main/java/Model/Requests/RequestEditAuction.java
+++ src/main/java/Model/Requests/RequestEditAuction.java
@@ -2,28 +2,43 @@ package Model.Requests;
 
 import Model.Auction;
 import Model.Product;
+import Model.Users.Person;
 import Model.Users.Seller;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
 
-public class RequestEditAuction  {
-    private Seller seller;
+public class RequestEditAuction extends Request {
+    private String field;
     private Auction auction;
-    public static ArrayList<RequestEditAuction> allEditAuctionRequests = new ArrayList<RequestEditAuction>();
+    private String newChange;
 
-
-    public RequestEditAuction(Seller seller, Product product, Auction auction) {
-//        super(seller, product, auction);
-        this.seller = seller;
+    public RequestEditAuction(String type, String condition, Person sender, Auction auction, String field, String newChange) {
+        super(type, condition, sender);
+        this.field = field;
         this.auction = auction;
-        allEditAuctionRequests.add(this);
-    }
-
-    public Seller getSeller() {
-        return seller;
+        this.newChange = newChange;
     }
 
     public Auction getAuction() {
         return auction;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public String getNewChange() {
+        return newChange;
+    }
+
+    public void setChanges(String field) {
+        if (field.equals("start")) {
+            LocalTime localTime = LocalTime.of(Integer.parseInt(this.getNewChange().substring(0, 2)), Integer.parseInt(this.getNewChange().substring(3, 5)));
+            this.getAuction().setStart(localTime);
+        } else if (field.equals("end")) {
+            LocalTime localTime = LocalTime.of(Integer.parseInt(this.getNewChange().substring(0, 2)), Integer.parseInt(this.getNewChange().substring(3, 5)));
+            this.getAuction().setEnd(localTime);
+        }
     }
 }
