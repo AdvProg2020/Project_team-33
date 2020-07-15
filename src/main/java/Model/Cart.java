3e3eaebe74c8ac3f21e7a9faa9dbd84301d2c77b
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-07-15 23:53:39 +0430

    complete auction

--- src/main/java/Model/Cart.java
+++ src/main/java/Model/Cart.java
@@ -43,64 +43,9 @@ public class Cart {
     public double getMoneyForPurchase() {
         double money = 0;
         for (Product product : this.productsInCart) {
-            money += product.getMoney();
+            money += product.getMoney()-product.getDiscount();
         }
         return money;
     }
-
-    //    public ArrayList<BuyingProduct> getProductsInCart() {
-//        return productsInCart;
-//    }
-//
-//    public void removeProductFromCart(Product product) {
-//        this.productsInCart.remove(product);
-//    }
-//
-//    public Product getProductInCartById(int productID) {
-//        for (BuyingProduct product : this.productsInCart) {
-////            if (product.getProduct().getProductID() == productID) {
-//                return product.getProduct();
-////            }
-//        }
-//        return null;
-//    }
-//
-//    public void setProductInCart(Product productInCartById) {
-//    }
-//
-//        private HashMap<Product, Integer> userCart = new HashMap<Product, Integer>();
-//
-//    public double getPrice() {
-//        return 0;
-//    }
-//
-//    public boolean isProductAvailable(Product product) {
-//        return this.userCart.containsKey(product);
-//    }
-//
-//    public void addProductToCart(Product product, int num) {
-//        int newNum = num;
-//        if (this.isProductAvailable(product)) {
-//            newNum += this.userCart.get(product);
-//        }
-//        this.userCart.put(product, newNum);
-//    }
-//
-//    public void deleteProductFromCart(Product product) {
-//        this.userCart.remove(product);
-//    }
-//
-//    public HashMap<Product, Integer> getCart() {
-//        return this.userCart;
-//    }
-//
-//    public void decreaseNumberOfProduct(Product product, int num) {
-//        int newNum;
-//        newNum = this.userCart.get(product);
-//        if (newNum - num <= 0) deleteProductFromCart(product);
-//        else {
-//            this.userCart.put(product, newNum);
-//        }
-//    }
 
 }
