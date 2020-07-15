e70cf37cda986ee13ef9a3a8a24c07acf2827b52
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-07-15 18:52:39 +0430

    complete auction requests

--- src/main/java/Model/Requests/RequestAddAuction.java
+++ src/main/java/Model/Requests/RequestAddAuction.java
@@ -1,26 +1,22 @@
 package Model.Requests;
 
+
 import Model.Auction;
-import Model.Product;
+import Model.Users.Person;
 import Model.Users.Seller;
 
 import java.util.ArrayList;
 
-public class RequestAddAuction {
-    private Seller seller;
+public class RequestAddAuction extends Request {
     private Auction auction;
-    public static ArrayList<RequestAddAuction> allAddAuctionRequest = new ArrayList<RequestAddAuction>();
+    private static ArrayList<RequestAddAuction> requestAddAuctions = new ArrayList<>();
 
-    public RequestAddAuction(Seller seller, Product product, Auction auction) {
-//        super(seller, product, auction);
-        this.seller = seller;
+    public RequestAddAuction(String type, String condition, Person sender, Auction auction) {
+        super(type, condition, sender);
         this.auction = auction;
-        allAddAuctionRequest.add(this);
+        requestAddAuctions.add(this);
     }
 
-    public Seller getSeller() {
-        return seller;
-    }
 
     public Auction getAuction() {
         return auction;
