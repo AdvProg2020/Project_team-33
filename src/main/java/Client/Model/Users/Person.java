7e424d526b40581525fdabe4161ce6f1544f43c3
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2020-07-20 00:57:28 +0430

    change dataInputStream to json

--- src/main/java/Client/Model/Users/Person.java
+++ src/main/java/Client/Model/Users/Person.java
@@ -1,23 +1,27 @@
 package Client.Model.Users;
 
 import Server.Database.SaveData;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
 
 public class Person {
     protected String name, username, password, email, phone, family;
-    public static ArrayList<Person> people = new ArrayList<>();
-    SaveData saveData = new SaveData();
 
-    public Person(String username, String name, String family, String phone, String email, String password) {
+    @JsonCreator
+    public Person(@JsonProperty("username") String username, @JsonProperty("name") String name, @JsonProperty("family") String family, @JsonProperty("phone") String phone,
+                  @JsonProperty("email") String email, @JsonProperty("password") String password) {
         this.username = username;
         this.name = name;
         this.family = family;
         this.phone = phone;
         this.email = email;
         this.password = password;
-        people.add(this);
-//        saveData.addUser(this);
+    }
+
+    public Person() {
+
     }
 
 
@@ -64,40 +68,6 @@ public class Person {
 
     public String getPassword() {
         return this.password;
-    }
-
-
-    public static Person getPersonByUsername(String username) {
-        for (Person person : people) {
-            if (person.getUsername().equals(username)) {
-                return person;
-            }
-        }
-        return null;
-    }
-
-    public static boolean isAccountWithThisUsernameExist(String username) {
-        for (Person eachPerson : people) {
-            if (eachPerson.getUsername().equals(username)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static ArrayList<Person> getPeople() {
-        return people;
-    }
-
-    public static void deleteUser(Person person) {
-//        if (person instanceof Manager) {
-//            Manager.deleteUser(person);
-//        } else if (person instanceof Seller) {
-//            Seller.deleteUser(person);
-//        } else {
-//            Buyer.deleteUser(person);
-//        }
-        people.remove(person);
     }
 
     @Override
