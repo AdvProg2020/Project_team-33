79610fbb06c40f5494c5590c51eb863e51acdee6
mmdrez4 <mohammadreza.dn80@gmail.com>
2020-07-22 03:01:13 +0430

    update PublicSale.java in server

--- src/main/java/Server/Model/PublicSale.java
+++ src/main/java/Server/Model/PublicSale.java
@@ -1,7 +1,7 @@
 package Server.Model;
 
-import Client.Model.Users.Buyer;
-import Client.Model.Users.Seller;
+import Server.Model.Users.Buyer;
+import Server.Model.Users.Seller;
 
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -9,6 +9,8 @@ import java.util.HashMap;
 
 public class PublicSale {
     private LocalTime endTime;
+    public static int counter = 1;
+    private int id;
     private Seller seller;
     private Product product;
     private boolean isExpired;
@@ -22,15 +24,25 @@ public class PublicSale {
         this.seller = seller;
         participants= new HashMap<>();
         allPublicSales.add(this);
+        this.id = counter;
+        counter++;
     }
 
-    public static boolean isProductExpired(Product product){
+    public static boolean isProductExpired(Client.Model.Product product){
         for (PublicSale publicSale : allPublicSales) {
             if (publicSale.getProduct().equals(product)){
                 return publicSale.isExpired;
             }
         }
         return false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public boolean isExpired() {
